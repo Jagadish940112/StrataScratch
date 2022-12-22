50a38a5bfdf679672130723c3876ab44b06219bd
Jagadish940112 <116116336+Jagadish940112@users.noreply.github.com>
2022-12-22 19:55:58 +0800

    Create 9891. Customer Details.sql

---
+++ Easy/9891. Customer Details.sql
@@ -0,0 +1,12 @@
+/*
+Find the details of each customer regardless of whether the customer made an order.
+Output the customer's first name, last name, and the city along with the order details.
+You may have duplicate rows in your results due to a customer ordering several of the same items.
+Sort records based on the customer's first name and the order details in ascending order.
+*/
+
+SELECT first_name, last_name, city, order_details
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.cust_id
+ORDER BY first_name, order_details
