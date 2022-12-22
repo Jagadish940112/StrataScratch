9d31a721c3800d73b8466f6e9524c5f35d8f0127
Jagadish940112 <116116336+Jagadish940112@users.noreply.github.com>
2022-12-22 16:11:35 +0800

    Create 9924. Library Code.sql

---
+++ Easy/9924. Library Code.sql
@@ -0,0 +1,10 @@
+/*
+Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email.
+Output the library code.
+*/
+
+SELECT DISTINCT home_library_code
+FROM library_usage
+WHERE provided_email_address = 0 
+  AND circulation_active_year = 2016 
+  AND notice_preference_definition = 'email'
