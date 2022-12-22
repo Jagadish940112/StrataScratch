5e93945846493e783bc40c8a36e7d73bf9695123
Jagadish940112 <116116336+Jagadish940112@users.noreply.github.com>
2022-12-22 14:21:38 +0800

    Update 9992. Spotify ranking list.sql

--- Easy/9992. Spotify ranking list.sql
+++ Easy/9992. Spotify ranking list.sql
@@ -4,7 +4,7 @@ Output the artist name along with the corresponding number of occurrences.
 Order records by the number of occurrences in descending order.
 */
 
-SELECT artist, COUNT(artist) as number_of_occurrences
+SELECT artist, COUNT(artist) AS number_of_occurrences
 FROM spotify_worldwide_daily_song_ranking
 GROUP BY artist
 ORDER BY number_of_occurrences DESC
