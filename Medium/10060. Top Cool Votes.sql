4cc00585e40354d3dda53d58f0bef905bc1dca33
Jagadish940112 <116116336+Jagadish940112@users.noreply.github.com>
2022-12-29 05:07:11 +0800

    Create 10060. Top Cool Votes.sql

---
+++ Medium/10060. Top Cool Votes.sql
@@ -0,0 +1,9 @@
+/*
+Find the review_text that received the highest number of  'cool' votes.
+Output the business name along with the review text with the highest numbef of 'cool' votes.
+*/
+
+SELECT business_name, review_text
+FROM yelp_reviews
+WHERE cool = (SELECT MAX(cool)
+              FROM yelp_reviews)
